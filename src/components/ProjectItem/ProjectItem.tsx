64c7d125a5f53afdbf64f0b3909e392646549b76
Ugne <u.adomaityte98@gmail.com>
2023-12-28 18:35:35 +0200

    update ProjectItem component style

--- src/components/ProjectItem/ProjectItem.tsx
+++ src/components/ProjectItem/ProjectItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ProjectItem.scss';
 
 export interface ProjectItemProps {
     id: number;
@@ -11,8 +12,8 @@ const ProjectItem: React.FC<ProjectItemProps> = ({ title, description, imageUrl 
     return (
         <div className="project-item">
             <img src={imageUrl} alt={title} />
-            <h2>{title}</h2>
-            <p>{description}</p>
+            <h2 className='title small'>{title}</h2>
+            <p className='text small'>{description}</p>
         </div>
     );
 };
--- src/components/ProjectItem/ProjectsList.tsx
+++ src/components/ProjectItem/ProjectsList.tsx
@@ -7,7 +7,7 @@ interface ProjectsListProps {
 
 const ProjectsList: React.FC<ProjectsListProps> = ({ projects }) => {
     return (
-        <div className='projects-list'>
+        <div className='projects-list grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
             {projects.map((project) => (
                 <ProjectItem
                     key={project.id}
