220b9fee816b0530505da5c31c38115c45133663
Ugne <u.adomaityte98@gmail.com>
2024-01-02 22:54:30 +0200

    add PostItem component style

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import './PostItem.scss';
+import Button from '../Button/Button';
 
 export interface PostItemProps {
   id: number;
@@ -10,9 +12,12 @@ export interface PostItemProps {
 const PostItem: React.FC<PostItemProps> = ({ title, date, excerpt }) => {
   return (
     <div className="post-item">
-      <h2 className="post-title">{title}</h2>
-      <p className="post-date">{date}</p>
-      <p className="post-excerpt">{excerpt}</p>
+      <h2 className="post-title title title-small">{title}</h2>
+      <p className="post-excerpt text text-small">{excerpt}</p>
+      <div className='post-bottom-box flex justify-between pt-7'>
+        <p className="post-date text text-small pt-1">{date}</p>
+        <Button>Read</Button>
+      </div>
     </div>
   );
 };
--- src/components/PostItem/PostsList.tsx
+++ src/components/PostItem/PostsList.tsx
@@ -6,8 +6,8 @@ interface PostsListProps {
 }
 
 const PostsList: React.FC<PostsListProps> = ({ posts }) => {
-  return (
-    <div className="posts-list">
+  return ( 
+    <div className="posts-list grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
       {posts.map(post => (
         <PostItem 
           key={post.id} 
