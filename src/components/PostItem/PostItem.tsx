19793d0bd8592f3cb06033d001c199e6f316c516
Ugne <u.adomaityte98@gmail.com>
2024-01-07 01:23:18 +0200

    fetch blog posts data using hasnode api

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -3,20 +3,21 @@ import './PostItem.scss';
 import Button from '../Button/Button';
 
 export interface PostItemProps {
-  id: number;
+  id: string;
   title: string;
   date: string;
   excerpt: string;
+  url: string;
 }
 
-const PostItem: React.FC<PostItemProps> = ({ title, date, excerpt }) => {
+const PostItem: React.FC<PostItemProps> = ({ title, date, excerpt, url }) => {
   return (
     <div className="post-item">
       <h2 className="post-title title title-small">{title}</h2>
       <p className="post-excerpt text text-small">{excerpt}</p>
       <div className='post-bottom-box flex justify-between pt-7'>
         <p className="post-date text text-small pt-1">{date}</p>
-        <Button>Read</Button>
+        <Button href={url}>Read</Button>
       </div>
     </div>
   );
--- src/components/PostItem/PostsList.tsx
+++ src/components/PostItem/PostsList.tsx
@@ -1,17 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PostItem, {PostItemProps} from './PostItem';
 
-interface PostsListProps {
-  posts: PostItemProps[];
-}
+const PostsList: React.FC = () => {
+  const [posts, setPosts] = useState<PostItemProps[]>([]);
 
-const PostsList: React.FC<PostsListProps> = ({ posts }) => {
-  return ( 
+  useEffect(() => {
+    fetch('https://api.hashnode.com/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          {
+            user(username: "ugnead") {
+              publication {
+                posts {
+                  _id
+                  title
+                  brief
+                  dateAdded
+                  url
+                }
+              }
+            }
+          }
+        `,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        const extractedPosts = data.data.user.publication.posts.map((post: any) => ({
+          id: post._id,
+          title: post.title,
+          date: new Date(post.dateAdded).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' }),
+          excerpt: post.brief,
+          url: post.url
+        }));
+        setPosts(extractedPosts);
+      })
+      .catch(error => console.error('Error fetching Hashnode posts:', error));
+  }, []);
+
+  return (
     <div className="posts-list grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 pb-10">
       {posts.map(post => (
         <PostItem 
-          key={post.id} 
-          {...post}
+          id={post.id} 
+          title={post.title}
+          date={post.date}
+          excerpt={post.excerpt}
+          url={post.url}
         />
       ))}
     </div>
