a64cb9324b26cc73802238cb40f97bd30f8bd9d0
Ugne <u.adomaityte98@gmail.com>
2024-01-13 18:12:53 +0200

    update Navigation style and logic

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -8,17 +8,22 @@ interface NavigationProps {
 const Navigation: React.FC<NavigationProps> = ({ activeSection }) => {
   const handleNavLinkClick = (sectionId: string) => {
     const section = document.getElementById(sectionId);
-    section?.scrollIntoView({ behavior: 'smooth' });
+    if (section) {
+      const offsetTop = section.offsetTop - 150;
+      window.scrollTo({
+        top: offsetTop,
+        behavior: "smooth"
+      });
+    }
   };
 
   return (
     <nav className="navigation">
       <ul className="flex justify-center space-x-8 title title-small">
-        <li className={`${activeSection === 'about' ? 'active-nav-item' : ''}`} onClick={() => handleNavLinkClick('about')}>About</li>
-        <li className={`${activeSection === 'skillset' ? 'active-nav-item' : ''}`} onClick={() => handleNavLinkClick('skillset')}>Skillset</li>
-        <li className={`${activeSection === 'experience' ? 'active-nav-item' : ''}`} onClick={() => handleNavLinkClick('experience')}>Experience</li>
-        <li className={`${activeSection === 'projects' ? 'active-nav-item' : ''}`} onClick={() => handleNavLinkClick('projects')}>Projects</li>
-        <li className={`${activeSection === 'posts' ? 'active-nav-item' : ''}`} onClick={() => handleNavLinkClick('posts')}>Posts</li>
+        <li className={`nav-item ${activeSection === 'header' ? 'active-nav-item' : ''}`} onClick={() => handleNavLinkClick('header')}>Home</li>
+        <li className={`nav-item ${activeSection === 'about' ? 'active-nav-item' : ''}`} onClick={() => handleNavLinkClick('about')}>About</li>
+        <li className={`nav-item ${activeSection === 'projects' ? 'active-nav-item' : ''}`} onClick={() => handleNavLinkClick('projects')}>Projects</li>
+        <li className={`nav-item ${activeSection === 'posts' ? 'active-nav-item' : ''}`} onClick={() => handleNavLinkClick('posts')}>Posts</li>
       </ul>
     </nav>
   );
