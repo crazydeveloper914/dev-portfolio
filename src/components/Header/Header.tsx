fa56e9e7de0b571dd57632a0f887e4200ce2e8d5
Ugne <u.adomaityte98@gmail.com>
2024-01-16 23:09:55 +0200

    minor content and style update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,11 +8,11 @@ interface HeaderProps {
 const Header = forwardRef<HTMLElement, HeaderProps>(({ id }, ref) => (
   <header id={id} ref={ref} className="flex flex-col items-center justify-center h-[93vh]">
     <div className="typewriter">
-      <h1 className="main-title px-3">
+      <h1 className="main-title px-3 pb-3">
         Hi, <span>I&apos;m Ugne</span>
       </h1>
     </div>
-    <p className="delayedText text text-medium text-center">A Passionate Full Stack Developer</p>
+    <p className="delayedText text text-medium text-center">A Full Stack Developer</p>
   </header>
 ));
 
